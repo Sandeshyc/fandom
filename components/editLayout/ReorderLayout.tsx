351386a7191115a64033151798b2aa56bb7e03fd
Benjamin Abraham <babraham@abs-cbn.com>
2023-06-28 18:44:35 +0530

    Made changes in the alignment and the buttons

--- components/editLayout/ReorderLayout.tsx
+++ components/editLayout/ReorderLayout.tsx
@@ -3,7 +3,7 @@ import { XMarkIcon } from '@heroicons/react/24/outline';
 import Reorder from '@/components/elements/Reorder';
 import useCurrentPageStore, { layoutType } from '@/hooks/useCurrentPageStore';
 import { Button } from '@mui/material';
-import { Event as EventIcon, BarChart as ShowChartIcon, Info as InfoIcon } from '@mui/icons-material';
+import { Event as EventIcon, BarChart as ShowChartIcon, Info as InfoIcon, Add as AddIcon } from '@mui/icons-material';
 
 const labelMap = [
   { key: 'title', label: 'Playlist' },
@@ -28,15 +28,20 @@ const ReorderLayout: React.FC<InfoModalProps> = ({ visible, onClose, title, topD
   const { currentLayout = {} as layoutType, setCurrentLayout } = useCurrentPageStore();
   const [currentTopData, setCurrentTopData] = useState(topData);
   const [addedItems, setAddedItems] = useState<any[]>([]); // New state to hold the added items
+  const [initialLayout, setInitialLayout] = useState<layoutType>(currentLayout);
+
+
 
   // use a callback function to update CurrentLayout
   const updateCurrentLayout = useCallback((movieLists: any[]) => {
     setCurrentLayout({ pageName: 'iwantTFCHome', items: movieLists });
-  }, [setCurrentLayout]);
+  }, [setCurrentLayout, initialLayout]);
+  
 
   useEffect(() => {
     setIsVisible(!!visible);
     setCurrentTopData(topData);
+    setInitialLayout(currentLayout);
   }, [visible, topData]);
 
   const handleClose = useCallback((e) => {
@@ -56,12 +61,16 @@ const ReorderLayout: React.FC<InfoModalProps> = ({ visible, onClose, title, topD
   }, [currentLayout.items]);
 
   const handleCancel = useCallback(() => {
-    setAddedItems([]);
+    setCurrentLayout(initialLayout); // Reset the current layout to initial state
+    setAddedItems([]); // Clear the added items
     setIsVisible(false);
+  
     setTimeout(() => {
       onClose();
     }, 300);
-  }, [onClose]);
+  }, [onClose, initialLayout, setCurrentLayout]);
+  
+  
 
   console.log("updatedPlaylist", currentLayout.items)
 
@@ -72,11 +81,14 @@ const ReorderLayout: React.FC<InfoModalProps> = ({ visible, onClose, title, topD
   return (
     <div style={{ width: "1200px", top: "17%", left: "0", right: "0", marginLeft: "auto", marginRight: "auto" }} className={`fixed z-[999] left-0 top-0 p-6 rounded-md shadow-lg overflow-hidden bg-black border-2 ${isVisible ? 'scale-100' : 'scale-0'}`}>
       <div style={{ color: "white", fontSize: "20px", fontWeight: "bold" }}>{title}</div>
-      <div style={{ marginRight: "25px" }} className="cursor-pointer absolute top-6 right-40">
-        <Button variant="contained" data-button="close" onClick={handleAdd}>Add</Button>
+      <div style={{ marginRight: "40px" }} className="cursor-pointer absolute top-6 right-40">
+        <Button style={{width:"100px"}} variant="contained" data-button="close" onClick={handleAdd}>
+        <AddIcon style={{marginRight:"5px"}}/>
+          
+          Add</Button>
       </div>
       <div className="cursor-pointer absolute top-6 right-20">
-        <Button variant="contained" data-button="cancel" onClick={handleCancel}>Cancel</Button>
+        <Button style={{width:"100px"}} variant="contained" data-button="cancel" onClick={handleCancel}>Cancel</Button>
       </div>
       <div onClick={handleClose} className="cursor-pointer absolute top-6 right-3 h-10 w-10 rounded-full bg-white bg-opacity-70 flex items-center justify-center">
         <XMarkIcon className="text-white w-6" data-button="close" />
