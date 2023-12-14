2701eb180814d2aa278d0ddaca151a6fa022bd2d
Mahmud <mahmud.sg@gmail.com>
2023-12-15 01:50:23 +0600

    single color change

--- components/elements/ProgressBar/index.tsx
+++ components/elements/ProgressBar/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ProgressBar({done} : {done: number}) {
   return (
     <div className="w-full bg-gray-100 rounded-full h-2 z-30 absolute bottom-0">
-        <div className="bg-blue-200 h-2 rounded-full" style={{width: done + "%"}}></div>
+        <div className="bg-blue-300 h-2 rounded-full" style={{width: done + "%"}}></div>
     </div>
   )
 }
