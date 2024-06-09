86d1ad46a4b3b6a6c0db5c1e4895b7678854abc5
Abu Saim <abusaim@Saims-MacBook-Pro.local>
2024-06-09 12:09:32 +0600

    add icon on popup

--- modules/elements/AlreadyPurchased.tsx
+++ modules/elements/AlreadyPurchased.tsx
@@ -1,16 +1,7 @@
 import React, { useEffect, useState } from "react";
-import {
-  signIn,
-  getCurrentUser,
-  resendSignUp,
-  confirmSignUp,
-} from "@/utils/cognitoAuth";
-import useUserInfo from "@/hooks/useUserInfo";
-import { useRouter } from "next/router";
+import {CheckCircleOutline} from "@mui/icons-material";
 import Modal from "@mui/material/Modal";
 import { Roboto } from "next/font/google";
-import OTPInput from "@/modules/Identities/OTPInput";
-import Text from "@/modules/Identities/Text";
 import { CloseOutlined } from "@mui/icons-material";
 
 const roboto = Roboto({
@@ -39,9 +30,15 @@ const AlreadyPurchased = () => {
             <CloseOutlined sx={{ fontSize: 28 }} className="text-[#454545]" />
           </div>
           <div className="p-4 pt-8 min-h-[250px] flex justify-center items-center">
-            <h3 className="text-[#5F576F] text-center text-2xl font-semibold mb-2">
-              You have already purchased this item.
-            </h3>
+            <div className="text-center">
+              <CheckCircleOutline 
+                sx={{ fontSize: 60 }} 
+                className="text-green-500 mb-2"
+              />
+              <h3 className="text-[#5F576F] text-center text-2xl font-semibold mb-2">
+                You have already purchased this item.
+              </h3>
+            </div>
           </div>
         </div>
       </Modal>
