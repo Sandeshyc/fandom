333400d9c0ca52f20f7622b8e2e96c82fbb1b1c9
Abu Saim <abusaim@Saims-MacBook-Pro.local>
2024-06-21 13:04:11 +0600

    add playbill

---
+++ pages/[section]/members/playbill.tsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { getEntitlementList } from "@/services/api";
+import { getAllowedItems } from "@/utils/getData";
+import Preloader from "@/modules/skeletons/Preloader";
+const contentId = "6641a3eba9e8e0ae2a7786b8";
+const Playbill = () => {
+    const router = useRouter();
+    const { section } = router.query;
+    const [isReady, setIsReady] = useState(false);
+    const [userId, setUserId] = useState("");
+    const [pageDirectory, setPageDirectory] = useState("");
+    useEffect(() => {
+        if (pageDirectory) {
+          const userInfo = window.localStorage.getItem("userInfo");
+          if (userInfo) {
+            const userInfoObj = JSON.parse(userInfo);
+            if (userInfoObj.sub) {
+              setUserId(userInfoObj.sub);
+              const _getEntitlementList = async () => {
+                const resEntitlement = await getEntitlementList(userInfoObj.sub);
+                // console.log("Response::::", resEntitlement);
+                if (resEntitlement.status === "success") {
+                  const allowedIds = getAllowedItems(resEntitlement?.data);
+                  if (Array.isArray(allowedIds) && allowedIds.length > 0) {
+                    allowedIds.map((allowItem: any) => {
+                      if (
+                        allowItem?.content?.contentId === contentId &&
+                        allowItem?.content?.pageDirectory === pageDirectory
+                      ) {
+                        setIsReady(true);
+                      } else {
+                        // console.log("Entitlement Not found!");
+                        // router.push(`/` + section);
+                        window.location.replace(`/` + section);
+                      }
+                    });
+                  } else {
+                    // console.log("Entitlement Not found!");
+                    // router.push(`/` + section);
+                    window.location.replace(`/` + section);
+                  }
+                } else {
+                //   console.log("Entitlement API found");
+                  // router.push(`/` + section);
+                  window.location.replace(`/` + section);
+                }
+              };
+              _getEntitlementList();
+            } else {
+                //   console.log("User not found::", userInfoObj);
+                // router.push(`/` + section);
+                const itemUrl = `/` + section + `/members/playbill`;
+                window.localStorage.setItem("itemUrl", itemUrl);
+                window.location.replace(`/login`);
+            }
+          } else {
+            // console.log("User not found:", userInfo);
+            // router.push(`/` + section);
+            const itemUrl = `/` + section + `/members/playbill`;
+            window.localStorage.setItem("itemUrl", itemUrl);
+            window.location.replace(`/login`);
+          }
+        }
+    }, [pageDirectory]);
+    useEffect(() => {
+        if (section) {
+          setPageDirectory(section + "/members");
+        }
+    }, [section]);
+    return (
+        <>
+        {isReady ? (
+            <div>
+                <h1>PayBill {section}</h1>
+            </div>
+        ) : (
+            <Preloader />
+          )}
+        </>
+    );
+};
+export default Playbill;
